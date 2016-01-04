563e9369e7d80a1f6fa252734ccbc3799bed9266
Patsy <dev.patsy.iv@gmail.com>
2016-01-04 17:33:22 -0600

    add coordenates to the table

--- Prototypes/table generator/script/script.js
+++ Prototypes/table generator/script/script.js
@@ -1,11 +1,13 @@
 
-
 function createTable() {
 
-    var rows = document.getElementById("setRows").value;
-    var cols = document.getElementById("setCols").value;
+    var rows           = document.getElementById("setRows").value;
+    var cols           = document.getElementById("setCols").value;
+    var xNegativeLimit = (cols / 2) - cols;
+    var yPositiveLimit = rows / 2;
+    var xAxisPosition;
+    var yAxisPosition;
     var table;
-
 
     table = "<table>";
     table += "<tbody>";
@@ -14,15 +16,17 @@ function createTable() {
          table += "<tr>";
 
          for (var j = 0; j < cols; j++) {
-            table += "<td></td>";
+            xAxisPosition = xNegativeLimit + j;
+            yAxisPosition = yPositiveLimit - i;
+            table += "<td>" + xAxisPosition + "," + yAxisPosition + "</td>";
          };
-         table += "</tr>";
+         table +="</tr>";
      }; 
 
     table += "</table>";
     table += "</tbody>";
 
     document.getElementById("tableHolder").innerHTML= table;
+  
 }
-
 
