280128cafcc345d147e5484cf1d503c2c1d4ac26
Patsy <dev.patsy.iv@gmail.com>
2016-02-04 11:52:11 -0600

    add new bottom rows

--- Prototypes/table-questions/script/script-new-rows.js
+++ Prototypes/table-questions/script/script-new-rows.js
@@ -1,6 +1,6 @@
 
 function addTopRow() {
-	var theTable, newTopRow;
+	var theTable, newTopRow, firstCells;
 
 	var theTable  = document.getElementById('myTable');
 	var newTopRow = theTable.insertRow(0);
@@ -13,6 +13,28 @@ function addTopRow() {
 		var lastCoor  = firstCells[i].innerHTML.split(",");
 		var newXasis = parseInt(lastCoor[0]);
 		var newtYasis = parseInt(lastCoor[1]) + 1;
+
+		newCells.innerHTML = newXasis + ',' + newtYasis;
+	};
+}
+
+
+function addBottomRow() {
+	var theTable, rowsLenght, firstCells, newTopRow;
+
+	var theTable   = document.getElementById('myTable');
+	var rowsLenght = theTable.getElementsByTagName('tr').length - 1;
+	console.log(rowsLenght);
+	var firstCells = theTable.getElementsByTagName('tr')[rowsLenght].getElementsByTagName("td");
+	var newTopRow  = theTable.insertRow(-1);
+
+	for (var i = 0; i < firstCells.length; i++) {
+		var newCells, lastCoor, newXasis, newtYasis;
+
+		var newCells = newTopRow.insertCell(i);
+		var lastCoor  = firstCells[i].innerHTML.split(",");
+		var newXasis = parseInt(lastCoor[0]);
+		var newtYasis = parseInt(lastCoor[1]) - 1;
 
 		newCells.innerHTML = newXasis + ',' + newtYasis;
 	};
