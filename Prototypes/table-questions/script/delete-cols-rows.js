68584e85b7bba1c073bbb8f5dd10367e4dc86982
Patsy <dev.patsy.iv@gmail.com>
2016-02-09 13:05:29 -0600

    prototype for delete cols and rows complete

---
+++ Prototypes/table-questions/script/delete-cols-rows.js
@@ -0,0 +1,111 @@
+
+function addTopRow() {
+    var theTable, newTopRow, firstCells;
+
+    var theTable  = document.getElementById('myTable');
+    var newTopRow = theTable.insertRow(0);
+    var firstCells  = theTable.getElementsByTagName('tr')[1].getElementsByTagName("td");
+
+    for (var i = 0; i < firstCells.length; i++) {
+        var newCells, lastCoor, newXasis, newtYasis;
+
+        var newCells = newTopRow.insertCell(i);
+        var lastCoor  = firstCells[i].innerHTML.split(",");
+        var newXasis = parseInt(lastCoor[0]);
+        var newtYasis = parseInt(lastCoor[1]) + 1;
+
+        newCells.innerHTML = newXasis + ',' + newtYasis;
+    };
+
+    deleteTopRow();
+}
+
+
+function addBottomRow() {
+    var theTable, rowsLenght, firstCells, newTopRow;
+
+    var theTable   = document.getElementById('myTable');
+    var rowsLenght = theTable.getElementsByTagName('tr').length - 1;
+    var firstCells = theTable.getElementsByTagName('tr')[rowsLenght].getElementsByTagName("td");
+    var newTopRow  = theTable.insertRow(-1);
+
+    for (var i = 0; i < firstCells.length; i++) {
+        var newCells, lastCoor, newXasis, newtYasis;
+
+        var newCells = newTopRow.insertCell(i);
+        var lastCoor  = firstCells[i].innerHTML.split(",");
+        var newXasis = parseInt(lastCoor[0]);
+        var newtYasis = parseInt(lastCoor[1]) - 1;
+
+        newCells.innerHTML = newXasis + ',' + newtYasis;
+    };
+
+    deleteBottomRow()
+}
+
+function addLeftCol() {
+    var rowsTable;
+
+    var rowsTable = document.getElementById('myTable').getElementsByTagName('tr');
+
+    for (var i = 0; i < rowsTable.length; i++) {
+        var newLeftCol, leftLatsCoor, lastCoor, newXasis, newtYasis;
+
+        var newLeftCol    = rowsTable[i].insertCell(0);
+        var leftLatsCoor  = rowsTable[i].getElementsByTagName('td')[1];
+        var lastCoor      = leftLatsCoor.innerHTML.split(",");
+        var newXasis      = parseInt(lastCoor[0]) - 1;
+        var newtYasis     = parseInt(lastCoor[1]);
+
+        newLeftCol.innerHTML = newXasis + ',' + newtYasis;
+    };
+
+    deleteLeftCol()
+}
+
+function addRightCol() {
+    var rowsTable, colslenght;
+
+    var rowsTable = document.getElementById('myTable').getElementsByTagName('tr');
+    var colslenght    = rowsTable[0].getElementsByTagName('td').length - 1;
+
+    for (var i = 0; i < rowsTable.length; i++) {
+        var newRightCol, rightLatsCoor, lastCoor, newXasis, newtYasis;
+
+        var newRightCol   = rowsTable[i].insertCell(-1);
+        var rightLatsCoor = rowsTable[i].getElementsByTagName('td');
+        var lastCoor      = rightLatsCoor[colslenght].innerHTML.split(",");
+        var newXasis      = parseInt(lastCoor[0]) + 1;
+        var newtYasis     = parseInt(lastCoor[1]);
+
+        newRightCol.innerHTML = newXasis + ',' + newtYasis;
+    };
+
+    deleteRightCol()
+}
+
+// In this part I will add the delete rows/cols functions
+
+function deleteTopRow() {
+    var theTable  = document.getElementById('myTable');
+    theTable.deleteRow(-1);
+}
+
+function deleteBottomRow() {
+    var theTable  = document.getElementById('myTable');
+    theTable.deleteRow(0);
+}
+
+function deleteLeftCol() {
+    var rowsTable = document.getElementById('myTable').getElementsByTagName('tr');
+    for (var i = 0; i < rowsTable.length; i++) {
+        rowsTable[i].deleteCell(-1);
+    };
+}
+
+function deleteRightCol() {
+    var rowsTable = document.getElementById('myTable').getElementsByTagName('tr');
+    for (var i = 0; i < rowsTable.length; i++) {
+        rowsTable[i].deleteCell(0);
+    };
+}
