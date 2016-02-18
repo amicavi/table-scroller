a9f5003afd84d5394071b472a96f5c2c82b2f5e5
Patsy <dev.patsy.iv@gmail.com>
2016-02-18 15:48:02 -0600

    module 4 deleting older rows and cols

--- script/script.js
+++ script/script.js
@@ -1,37 +1,59 @@
+// The main functions that are called by each button that in turn calls the specific functions that it needs
+
 function topLeftButton() {
     addTopRow();
     addLeftCol();
+
+    deleteBottomRow();
+    deleteRightCol();
 }
 
 function topButton() {
     addTopRow();
+
+    deleteBottomRow();
 }
 
 function topRightButton() {
     addTopRow();
     addRightCol();
+
+    deleteBottomRow();
+    deleteLeftCol();
 }
 
 function leftButton() {
     addLeftCol();
+
+    deleteRightCol();
 }
 
 function rightButton() {
     addRightCol();
+
+    deleteLeftCol();
 }
 
 function bottomLeftButton() {
     addBottomRow();
     addLeftCol();
+
+    deleteTopRow();
+    deleteRightCol();
 }
 
 function bottomButton() {
     addBottomRow();
+
+    deleteTopRow();
 }
 
 function bottomRightButton() {
     addBottomRow();
     addRightCol();
+
+    deleteTopRow();
+    deleteLeftCol();
 }
 
 // Functions that add the new rows and cols to the main table
@@ -77,5 +99,39 @@ function addLeftCol() {
 
     for (var i = 0; i < tableRows.length; i++) {
         tableRows[i].insertCell(0);
+    };
+}
+
+// funcitons that delete the opposite columns and rows to avoid the overflow
+
+function deleteTopRow() {
+    var theTable;
+
+    var theTable  = document.getElementById('main_table');
+    theTable.deleteRow(-1);
+}
+
+function deleteRightCol() {
+    var rowsTable;
+    
+    var rowsTable = document.getElementById('main_table').getElementsByTagName('tr');
+    for (var i = 0; i < rowsTable.length; i++) {
+        rowsTable[i].deleteCell(0);
+    };
+}
+
+function deleteBottomRow() {
+    var theTable;
+
+    var theTable  = document.getElementById('main_table');
+    theTable.deleteRow(0);
+}
+
+function deleteLeftCol() {
+    var rowsTable;
+
+    var rowsTable = document.getElementById('main_table').getElementsByTagName('tr');
+    for (var i = 0; i < rowsTable.length; i++) {
+        rowsTable[i].deleteCell(-1);
     };
 }
